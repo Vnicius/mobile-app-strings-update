224c5ab65f48505d5902a3cbc806355b3a62707d
Vinícius <vinicius.matheus252@gmail.com>
2021-11-16 13:45:51 -0300

    Remove pull request

--- entrypoint.sh
+++ entrypoint.sh
@@ -43,22 +43,22 @@ find "$INPUT_STRINGS_PATH" -type f -name "$INPUT_STRINGS_FILE_NAME" | while read
     mkdir -p $dest && cp $src_file $dest_file
 done
 
-cd "${INPUT_APP_PATH}"
+# cd "${INPUT_APP_PATH}"
 
-git config user.name 'Strings update'
-git config user.email 'action@github.com'
-git checkout -b ${INPUT_PR_BRANCH}
-git add .
+# git config user.name 'Strings update'
+# git config user.email 'action@github.com'
+# git checkout -b ${INPUT_PR_BRANCH}
+# git add .
 
-# Check if has changes to commit
-if [[ `git status --porcelain` ]]; then
-    git commit -m "${INPUT_PR_COMMIT}"
-    git push origin ${INPUT_PR_BRANCH}
-    hub pull-request \
-        --base ${INPUT_APP_BRANCH} \
-        --head ${INPUT_PR_BRANCH} \
-        -m "${INPUT_PR_TITLE}" \
-        -m "${INPUT_PR_BODY}" 
-else
-    echo "No changes"
-fi;
+# # Check if has changes to commit
+# if [[ `git status --porcelain` ]]; then
+#     git commit -m "${INPUT_PR_COMMIT}"
+#     git push origin ${INPUT_PR_BRANCH}
+#     hub pull-request \
+#         --base ${INPUT_APP_BRANCH} \
+#         --head ${INPUT_PR_BRANCH} \
+#         -m "${INPUT_PR_TITLE}" \
+#         -m "${INPUT_PR_BODY}" 
+# else
+#     echo "No changes"
+# fi;
